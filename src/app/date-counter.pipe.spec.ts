20da0f7e3d9672490c5be2917b149397f0d51f63
iankurao <kuraoian@gmail.com>
2019-04-29 15:53:24 +0300

    add pipes spec file

---
+++ src/app/date-counter.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { DateCountPipe} from './date-counter.pipe';
+
+describe('DateCountPipe', () => {
+  it('should create an instance', () => {
+    expect(new DateCountPipe()).toBeTruthy();
+  });
+});
