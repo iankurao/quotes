e01cc0c030b05153415d1c982ef639099d0c06b0
iankurao <kuraoian@gmail.com>
2019-04-29 15:07:30 +0300

    Add forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';undefined;
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './quote-form/form.component';
+import { HighlightDirective } from './highlights.directive';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { DateCountPipe } from './date-counter.pipe';
+import { QuoteDisplayComponent } from './quote/quote-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    HighlightDirective,
+    QuoteDetailsComponent,
+    DateCountPipe,
+    QuoteDisplayComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
+
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
