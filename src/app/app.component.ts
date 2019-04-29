78de61d9b9a71be8576c06a65ba1225991227af8
iankurao <kuraoian@gmail.com>
2019-04-29 15:18:16 +0300

    add delete quote function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,6 +32,18 @@ addNewquote(quote){
 
 
 
+    deleteGoal(isComplete,index){
+      if (isComplete){
+          let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index]}`)
+          
+          if(toDelete){
+              this.quotes.splice(index,1)
+          }
+      }
+
+  }
+
+
 }
 
 
