a2dc87da74705462378e47a92d281765012ec258
iankurao <kuraoian@gmail.com>
2019-04-29 14:57:46 +0300

    add quote

---
+++ src/app/quote/quote-display.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-quote-display',
+  templateUrl: './quote-display.component.html',
+  styleUrls: ['./quote-display.component.css']
+})
+ 
+export class QuoteDisplayComponent implements OnInit{
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
