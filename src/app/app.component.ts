f9ea7ed86e7872bff3d6ba4b5e311c5c6ba6bad2
iankurao <kuraoian@gmail.com>
2019-04-29 15:16:46 +0300

    customize app to register upvotes and downvotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+
 import { Component } from '@angular/core';
+import { Quote } from './quote';
+import { update } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quotes';
+  voters:number[]=[0,0] ;
+  upvotes:number = 0 ;
+  downvotes:number = 0 ;
+  quotes = [
+        new Quote('Xystus','Politics is a strong and slow boring of hard boards',new Date(2019,1,25)),
+        new Quote('Mawarks','Never give up till the end',new Date(2019,1,6)),
+        new Quote('Ian','Work hard in silence,,Let your lamborghini make the noise',new Date(2019,1,24)),
+    ]
+    upvote(){
+      this.upvotes +=1 ;
 }
+downvote(){
+  this.downvotes +=1 ;
+}
+
+addNewquote(quote){
+        this.quotes.unshift(quote)
+    }
+
+
+
+
+}
+
+
+
+
